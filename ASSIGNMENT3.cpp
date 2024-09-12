0fe4919cc6a9b2f46f304c465e6566dafa55749f
SAHIL DHAS <sahildhas2005@gmail.com>
2024-09-12 10:14:42 +0530

    Update ASSIGNMENT3.cpp

--- ASSIGNMENT3.cpp
+++ ASSIGNMENT3.cpp
@@ -3,84 +3,80 @@ Name:SAHIL DHAS
 Roll No. 23514
 Sub :Data Structures and algorithms
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
-#define MAXSIZE 3
-int rear = -1,front=-1,queue[MAXSIZE];
+#define MAXSIZE 5
+int rear = -1, front = -1, queue[MAXSIZE];
 
-int insert()
-{
-	int x ;
-	if(front==(rear+1)%MAXSIZE)
-	{
-		cout<<"Queue is full ";
-	}
-	else
-	{
-		if(front==-1)
-		{
-			front=0;
-			cout<<front;
-		}
-		rear = (rear+1)%MAXSIZE;
-		cout<<"enter a number to enter :  ";
-		cin>>x;
-		queue[rear]=x ;
-		cout<<x<<" is added at : "<<rear<<endl;
-	}
-}
-int Delete()
-{
-	if((front==-1)||(front>rear))
-	{
-		cout<<"Queue is Empty";
-	}
-	
-	else
-	{
-		int ele ;
-		ele = queue[front];
-		cout<<ele<<" is deleted at : "<<front<<endl;
-	if(rear==(MAXSIZE-1)&&(front==0)||(front>rear))
-	{
-		front=-1;
-		rear=-1;
-	}
-	}
-}
-int display()
-{
-	for(int i = front;i<=rear;i++)
-	{
-		cout<<queue[i]<<endl;
-	}
+void insert() {
+    int x;
+    if ((rear + 1) % MAXSIZE == front) {
+        cout << "Queue is full" << endl;
+        return;
+    }
+    
+    if (front == -1) {
+        front = 0;
+    }
+    
+    rear = (rear + 1) % MAXSIZE;
+    cout << "Enter a number to insert: ";
+    cin >> x;
+    queue[rear] = x;
+    cout << x << " is added at position: " << rear << endl;
 }
 
-int main()
-{
-	while(!(front== MAXSIZE))
-	{
-		cout<<"ENTER WHAT YOU WANT : \n 1.INSERT \n 2.DELETE \n 3.DISPLAY"<<endl;
-		int x; 
-		cout<<": ";
-		cin>>x;
-		if(x==1)
-		{
-			insert();
-			continue;
-		}
-		if(x==2)
-		{
-			Delete();
-			continue;
-		}
-		if(x==3)
-		{
-			display();
-		}
-		else
-		{
-			cout<<"enter valid choice "<<endl;
-		}
-	}
+void Delete() {
+    if (front == -1) {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+    
+    int ele = queue[front];
+    cout << ele << " is deleted from position: " << front << endl;
+    
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % MAXSIZE;
+    }
+}
+
+void display() {
+    if (front == -1) {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+    
+    int i = front;
+    while (true) {
+        cout << queue[i] << endl;
+        if (i == rear) break;
+        i = (i + 1) % MAXSIZE;
+    }
+}
+
+int main() {
+    int choice;
+    while (true) {
+        cout << "Enter what you want: \n1. INSERT \n2. DELETE \n3. DISPLAY \n4. EXIT" << endl;
+        cout << ": ";
+        cin >> choice;
+        
+        switch (choice) {
+            case 1:
+                insert();
+                break;
+            case 2:
+                Delete();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+            default:
+                cout << "Enter a valid choice" << endl;
+        }
+    }
 }
