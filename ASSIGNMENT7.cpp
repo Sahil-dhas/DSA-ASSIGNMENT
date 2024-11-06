c3d9a68a7dde18a657201e44e972a864a05ed8e0
SAHIL DHAS <sahildhas2005@gmail.com>
2024-11-07 00:29:50 +0530

    Update and rename dsa assignment 7 final.cpp to ASSIGNMENT7.cpp

--- ASSIGNMENT7.cpp
+++ ASSIGNMENT7.cpp
@@ -1,12 +1,12 @@
 /*DSA ASSIGN NO.7
-*Name: Paras Deshpande
-*Roll no. 23519[s2]
+*Name: sahil dhas
+*Roll no. 23514[s1]
 *Problem Statement:
 *Represent a graph of your college campus using adjacency list /adjacency matrix. Nodes should 
 *represent the various departments/institutes and links should represent the distance between them. 
 *Find minimum spanning tree
-*a) Using Kruskal’s algorithm.
-*b) Using Prim’s algorithm.
+*a) Using Kruskalâ€™s algorithm.
+*b) Using Primâ€™s algorithm.
 */
 #include<iostream>
 using namespace std;
